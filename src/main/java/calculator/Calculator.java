77b37cf765c7dfb6e681da719c49c99cd3843385
rachit <rachitban20@gmail.com>
2020-05-09 01:40:37 +0530

    static input

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,38 +1,17 @@
 package calculator;
-import java.util.Scanner; 
 
 public class Calculator {
 
 	public static void main(String[] args){
-	Scanner sc=new Scanner(System.in);
-	int a,b,choice=1;	
-	while(choice!=5){
-		System.out.println("Enter your choice: ");
-		System.out.println("Add : 1");
-		System.out.println("Subtract : 2");
-		System.out.println("Multiply : 3");
-		System.out.println("Divide : 4");
-		System.out.println("Exit : 5");
-		choice=sc.nextInt();
-		if(choice==5)
-			break;
-
-		System.out.println("Enter two numbers ");
-		a=sc.nextInt();
-		b=sc.nextInt();
-		if(choice==1)
-		    System.out.println("Result " + add(a,b));
+	int a,b;
 		
-		if(choice==2)
-		    System.out.println("Result " + subtract(a,b));
-
-		if(choice==3)
-		    System.out.println("Result " + multiply(a,b));
+	System.out.println("20 + 6 = " + add(20,6));
 		
-		if(choice==4)
-		    System.out.println("Result " + divide(a,b));
-			
-	}
+	System.out.println("10 - 9 = " + subtract(10,9));
+
+	System.out.println("6 * 7 = " + multiply(6,7));
+		
+	System.out.println("20 / 10 = " + divide(20,10));
 
 	}
 
